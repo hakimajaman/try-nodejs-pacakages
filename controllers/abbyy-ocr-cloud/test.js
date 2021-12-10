0063ddeb6bd6832b5023152f8ca0792074783800
hakimajaman <rifqitaufiqulhakim199922@gmail.com>
2021-12-10 09:18:51 +0700

    feat: add abbyy

---
+++ controllers/abbyy-ocr-cloud/test.js
@@ -0,0 +1,105 @@
+// Demo sample using ABBYY Cloud OCR SDK from Node.js
+
+if (typeof process == "undefined" || typeof window != "undefined") {
+  throw new Error("This code must be run on server side under NodeJS");
+}
+
+// !!! Please provide your application id and password and remove this line !!!
+// To create an application and obtain a password,
+// register at https://cloud.ocrsdk.com/Account/Register
+// More info on getting your application id and password at
+// https://ocrsdk.com/documentation/faq/#faq3
+
+// Change to http://cloud-westus.ocrsdk.com for applications created in US location
+// Change to https for secure connection
+var serviceUrl = "http://cloud-eu.ocrsdk.com/";
+// Name of application you created
+var appId = process.env.ABBYY_OCR_CLOUD_APP_ID;
+// Password should be sent to your e-mail after application was created
+var password = process.env.ABBYY_OCR_CLOUD_PASSWORD;
+
+var imagePath = "./public/assets/image-cert.jpeg";
+var outputPath = "./public/assets/output-2.xml";
+
+const runAbbyy = (req, res) => {
+  try {
+    console.log("ABBYY Cloud OCR SDK Sample for Node.js");
+
+    var ocrsdkModule = require("./ocrsdk.js");
+    var ocrsdk = ocrsdkModule.create(appId, password, serviceUrl);
+
+    if (appId.length == 0 || password.length == 0) {
+      throw new Error("Please provide your application id and password!");
+    }
+
+    if (imagePath == "myFile.jpg") {
+      throw new Error("Please provide path to your image!");
+    }
+
+    function downloadCompleted(error) {
+      if (error) {
+        console.log("Error: " + error.message);
+        return;
+      }
+      console.log("Done.");
+    }
+
+    function processingCompleted(error, taskData) {
+      if (error) {
+        console.log("Error: " + error.message);
+        return;
+      }
+
+      if (taskData.status != "Completed") {
+        console.log("Error processing the task.");
+        if (taskData.error) {
+          console.log("Message: " + taskData.error);
+        }
+        return;
+      }
+
+      console.log("Processing completed.");
+      console.log("Downloading result to " + outputPath);
+
+      ocrsdk.downloadResult(
+        taskData.resultUrl.toString(),
+        outputPath,
+        downloadCompleted
+      );
+    }
+
+    function uploadCompleted(error, taskData) {
+      if (error) {
+        console.log("Error: " + error.message);
+        return;
+      }
+
+      console.log("Upload completed.");
+      console.log(
+        "Task id = " + taskData.id + ", status is " + taskData.status
+      );
+      if (!ocrsdk.isTaskActive(taskData)) {
+        console.log("Unexpected task status " + taskData.status);
+        return;
+      }
+
+      ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+    }
+
+    var settings = new ocrsdkModule.ProcessingSettings();
+    // Set your own recognition language and output format here
+    settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
+    settings.exportFormat = "xml"; // All possible values are listed in 'exportFormat' parameter description
+    // at https://ocrsdk.com/documentation/apireference/processImage/
+
+    console.log("Uploading image..");
+    ocrsdk.processImage(imagePath, settings, uploadCompleted);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.log("Error: " + err.message);
+  }
+};
+
+module.exports = {
+  runAbbyy,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const { runAbbyy } = require("../controllers/abbyy-ocr-cloud/test");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/abbyy-ocr-cloud", runAbbyy);
 
 module.exports = router;
